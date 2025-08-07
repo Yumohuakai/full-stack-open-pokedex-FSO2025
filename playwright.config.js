8394f11db3dd45a70c4e7dad16a9ae0c0cee0efe
yumohuakai <yumohuakai@gmail.com>
2025-08-07 14:15:27 +0300

    FSO-part11-Exercise 11.9 Simple end-to-end tests: playwright is chosen for this exercise: fix the eslint error

--- playwright.config.js
+++ playwright.config.js
@@ -1,14 +1,14 @@
 import { defineConfig } from '@playwright/test'
 
 module.exports = defineConfig({
-  testDir: "./e2e-tests",
+  testDir: './e2e-tests',
   webServer: {
-    command: "npm run start-prod",
-    url: "http://localhost:5000",
+    command: 'npm run start-prod',
+    url: 'http://localhost:5000',
     timeout: 120 * 1000,
     reuseExistingServer: !process.env.CI,
   },
   use: {
-    baseURL: "http://localhost:5000/",
+    baseURL: 'http://localhost:5000/',
   },
-});
+})
