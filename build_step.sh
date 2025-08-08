722a0b8ed54b60a883b8e96be36f6e629f2ad249
yumohuakai <yumohuakai@gmail.com>
2025-08-08 22:33:21 +0300

    FSO-part11-Exercise 11.10 Deploying your application to Render - remove debugging code from build_step.sh

--- build_step.sh
+++ build_step.sh
@@ -1,11 +1,6 @@
 #!/bin/bash
-
 echo "Build script"
-echo "Content of server directory before build step"
-ls
 
 # add the commands here
 npm ci
 npm run build
-echo "Content of server directory after build step"
-ls
